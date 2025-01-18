67fdba1801844ac816ed8b28f10aa7c8912001d8
Khoa Nguyen <56934155+khoavnguyen03@users.noreply.github.com>
2025-01-18 15:15:49 -0800

    Readded envelope

--- envelope.py
+++ envelope.py
@@ -1,5 +1,19 @@
-class envelope:
-
-
-
+class Envelope: 
+    def __init__(self, name, parent, allocation): 
+        self.name = name
+        self.parent = parent # would pass in the name of the parent Envelope object
+        self.allocation = allocation 
+        self.exp_running_t =  allocation# upon creation, it will call the record function 
+        self.record() 
     
+    def record(self): 
+        data = [
+            []
+        ]
+        
+        # opening file then writing to it
+        csv_file_path = f'{self.name}.csv'
+        with open(csv_file_path, mode='w') as file: 
+            for row in data: 
+                pass 
+                # need to look at documentation to write this
