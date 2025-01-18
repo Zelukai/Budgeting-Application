8e3e231b2309f6870ac82258f44d254c4fb66be5
Fatehjot <gogiaf@oregonstate.edu>
2025-01-18 15:26:55 -0800

    Updated main

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import pandas
 import datetime
+
+
+#Lets create a basic UI
+
+while True:
+    print("Welcome to the Busy Budgeter")
+    print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
+    cLinput = input()
+    while (cLinput != 1 or cLinput != 2 or cLinput != 3):
+        print("Invalid option! Please enter a valid option (1, 2, or 3)")
+        cLinput = input()
+    
