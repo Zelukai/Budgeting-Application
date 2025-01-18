d48c4c2dcdcc995f8db52ce892007c7c09284d52
Fatehjot <gogiaf@oregonstate.edu>
2025-01-18 15:42:47 -0800

    updates

--- payment.py
+++ payment.py
@@ -1,15 +1,11 @@
 import envelope
-import datetime
 
 class Payment: 
-    def __init__(self, amount=0, date = datetime.now(), recurring = False, projection = False, envelope: Envelope):
+    def __init__(self, amount=0, recurring = False, projection = False, envelope: Envelope):
         self.amount = amount
-        self.date = date
         self.recurring = recurring
         self.projection = projection
         self.envelope = envelope
     def record(self):
-        csv_file_path = f'{self.envelope}.csv'
-        with open(csv_file_path, mode='w') as file: 
-            file.write([self.amount, self.date, self.envelope])
-    #see if this works
+        csv_file_path
+    
