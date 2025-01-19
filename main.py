f631e8ff61f2595f3ce7dfbe695cad63b8c7b493
Fatehjot <gogiaf@oregonstate.edu>
2025-01-18 16:14:31 -0800

    updates

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import envelope
 
 total = envelope.Envelope("origin", 5000)
 
-while True:
-    print("Welcome to the Busy Budgeter")
-    print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
-    cLinput = input()
-    while (cLinput != 1 or cLinput != 2 or cLinput != 3):
-        print("Invalid option! Please enter a valid option (1, 2, or 3)")
+def main():
+    while True:
+        total = envelope.Envelope("origin", 5000)
+        print("Welcome to the Busy Budgeter")
+        print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
         cLinput = input()
-    break
+        while (cLinput != 1 or cLinput != 2 or cLinput != 3):
+            print("Invalid option! Please enter a valid option (1, 2, or 3)")
+            cLinput = input()
+        break
