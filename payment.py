a60359ffc5be5c48fd8cdce43177dbd4e6628c62
Human from Somewhere <30worm@gmail.com>
2025-01-18 16:17:56 -0800

    payment record update

--- payment.py
+++ payment.py
@@ -1,11 +1,26 @@
 import envelope
+import datetime
+import csv
 
 class Payment: 
-    def __init__(self, amount=0, recurring = False, projection = False, envelope = None):
+    def __init__(self, amount=0, date = datetime.today(), recurring = False, projection = False, envelope = None):
         self.amount = amount
+        self.date = date
         self.recurring = recurring
         self.projection = projection
         self.envelope = envelope
+        self.record()
+
     def record(self):
-        csv_file_path
+        entryList = [self.amount, self.date, self.envelope]
+        csv_file_path = f'{self.envelope}.csv'
+        with open(csv_file_path, mode='w', newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(entryList)
+
+        # update user
+        print(f"Envelope {self.name} was created") 
+        print(f"Funds allocated: {self.allocation}")
+        print(f"Current total: {self.exp_running_t}")
+
     
