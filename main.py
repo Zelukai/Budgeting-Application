68d78b934e47c8474768ad9012457557cfd9056b
Fatehjot <gogiaf@oregonstate.edu>
2025-01-18 17:11:19 -0800

    updated main

--- main.py
+++ main.py
@@ -4,28 +4,31 @@ import envelope
 
 #Lets create a basic UI
 
+
+
+def match_input(cLinput=3):
+    match cLinput:
+        case '1':
+            print("Do something")
+        case '2':
+            print("Do something")
+        case '3':
+            print("Thank you for using this Busy Budgeter! This program is now ending...")
+
+
+
 total = envelope.Envelope("total", 5000)
 def main():
-    while True:
-        print("Welcome to the Busy Budgeter")
-        print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
+    print("Welcome to the Busy Budgeter")
+    print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
+    cLinput = input()
+    while cLinput not in ('1', '2', '3'):
+        print("Invalid option! Please enter a valid option (1, 2, or 3)")
         cLinput = input()
-        while cLinput not in ('1', '2', '3'):
-            print("Invalid option! Please enter a valid option (1, 2, or 3)")
-            cLinput = input()
-        match_input(cLinput)
-    
+    match_input(cLinput)
 
 
 if __name__ == "__main__":
     main()
 
 
-def match_input(cLinput=3):
-    match cLinput:
-        case 1:
-            print("Do something")
-        case 2:
-            print("Do something")
-        case 3:
-            print("Thank you for using this Busy Budgeter! This program is now ending...")
