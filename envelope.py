a91bdd3dc50d11192efe609a7f3d4c4faaa9701b
Khoa Nguyen <56934155+khoavnguyen03@users.noreply.github.com>
2025-01-18 16:01:42 -0800

    added children var

--- envelope.py
+++ envelope.py
@@ -1,10 +1,11 @@
 import csv
 
 class Envelope: 
-    def __init__(self, name, allocation, parent=None): 
+    def __init__(self, name, allocation, parent=None, children=[]): 
         self.name = name
         self.parent = parent 
         self.allocation = allocation 
+        self.children = children
         self.exp_running_t =  allocation# upon creation, it will call the record function 
         self.record() 
     
