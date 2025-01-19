30b0d759b4dc58621e4024aba79f1309460fbe39
Miguel <MiguelPimienta19@gmail.com>
2025-01-18 16:08:13 -0800

    khoa

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import datetime
 
 
 #Lets create a basic UI
+def main ():
 
-while True:
-    print("Welcome to the Busy Budgeter")
-    print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
-    cLinput = input()
-    while (cLinput != 1 or cLinput != 2 or cLinput != 3):
-        print("Invalid option! Please enter a valid option (1, 2, or 3)")
+    while True:
+        print("Welcome to the Busy Budgeter")
+        print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
         cLinput = input()
-    
+        while (cLinput != 1 or cLinput != 2 or cLinput != 3):
+            print("Invalid option! Please enter a valid option (1, 2, or 3)")
+            cLinput = input()
+        
