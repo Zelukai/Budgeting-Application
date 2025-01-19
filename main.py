e24ebd4653f0efef920bae72e60d055974a92021
Fatehjot <gogiaf@oregonstate.edu>
2025-01-18 16:52:04 -0800

    updated main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
         print("Welcome to the Busy Budgeter")
         print("What would you like to do? \n 1. Create an budgeting envelope \n 2. Enter a new payment \n 3. Exit Program")
         cLinput = input()
-        while (cLinput != 1 or cLinput != 2 or cLinput != 3):
+        while cLinput not in ('1', '2', '3'):
             print("Invalid option! Please enter a valid option (1, 2, or 3)")
             cLinput = input()
         break
