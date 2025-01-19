8294926b32a9e89a2c6a4e7644f2e45512f6e5b2
Khoa Nguyen <56934155+khoavnguyen03@users.noreply.github.com>
2025-01-18 16:30:23 -0800

    Payment addded to envelope record

--- envelope.py
+++ envelope.py
@@ -1,5 +1,6 @@
 import csv
 import payment
+import datetime
 
 class Envelope: 
     def __init__(self, name:str, allocation:int, parent=None, children=[]): 
@@ -18,8 +19,9 @@ class Envelope:
         with open(csv_file_path, mode='w', newline="") as file:
             writer = csv.writer(file)
             writer.writerow(headersList)
-            
         
+        allocation_pay = payment.Payment(self.allocation, datetime.today(), False, False, self.name)
+        allocation_pay.payment.record()
 
         # update user
         print(f"Envelope {self.name} was created") 
